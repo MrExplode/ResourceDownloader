14b6d0afb2724fc68ee76896485499ceeb92297f
MrExplode <pjanos@outlook.hu>
2018-08-05 19:07:06 +0200

    Added all champion names
    Some name might not work, i will test it

---
+++ src/me/mrexplode/resdownloader/Champions.java
@@ -0,0 +1,182 @@
+package me.mrexplode.resdownloader;
+
+public class Champions {
+	
+	public static final String[] champList = new String[] {
+			"Aatrox",
+			"Ahri",
+			"Akali",
+			"Alistar",
+			"Amumu",
+			"Anivia",
+			"Annie",
+			"Ashe",
+			"AurelionSol",
+			"Azir",
+			
+			"Bard",
+			"Blitzcrank",
+			"Brand",
+			"Braum",
+			
+			"Caitlyn",
+			"Camille",
+			"Cassiopeia",
+			//test it
+			"ChoGath",
+			"Corki",
+			
+			"Darius",
+			"Diana",
+			//test it
+			"DrMundo",
+			"Draven",
+			
+			"Ekko",
+			"Elsie",
+			"Evelynn",
+			"Ezreal",
+			
+			"Fiddlesticks",
+			"Fiora",
+			"Fizz",
+			
+			"Galio",
+			"Gankplank",
+			"Garen",
+			"Gnar",
+			"Gragas",
+			"Graves",
+			
+			"Hecarim",
+			"Heimerdinger",
+			
+			"Illaoi",
+			"Irelia",
+			//test it
+			"IVJarvan",
+			"Ivern",
+			
+			"Janna",
+			"Jax",
+			"Jayce",
+			"Jhin",
+			"Jinx",
+			
+			//test it
+			"Kaisa",
+			"Kalista",
+			"Karma",
+			"Karthus",
+			"Kassadin",
+			"Katarina",
+			"Kayle",
+			"Kayn",
+			"Kennen",
+			//test it
+			"Khazix",
+			"Kindred",
+			"Kled",
+			//test it
+			"Kogmaw",
+			
+			"LeBlanc",
+			"LeeSin",
+			"Leona",
+			"Lissandra",
+			"Lucian",
+			"Lulu",
+			"Lux",
+			
+			"Malphite",
+			"Malzahar",
+			"Maokai",
+			"MasterYi",
+			"MissFortune",
+			"Mordekaiser",
+			"Morgana",
+			
+			"Nami",
+			"Nasus",
+			"Nautilus",
+			"Nidalee",
+			"Nocturne",
+			"Nunu",
+			
+			"Olaf",
+			"Orianna",
+			"Ornn",
+			
+			"Pantheon",
+			"Poppy",
+			"Pyke",
+			
+			"Quinn",
+			
+			"Rakan",
+			"Rammus",
+			//test it
+			"RekSai",
+			"Renekton",
+			"Rengar",
+			"Riven",
+			"Rumble",
+			"Ryze",
+			
+			"Sejuani",
+			"Shaco",
+			"Shen",
+			"Shyvana",
+			"Singed",
+			"Sion",
+			"Sivir",
+			"Skarner",
+			"Sona",
+			"Soraka",
+			"Swain",
+			"Syndra",
+			
+			"TahmKench",
+			"Taliyah",
+			"Talon",
+			"Taric",
+			"Teemo",
+			"Tresh",
+			"Tristana",
+			"Trundle",
+			"Tryndamere",
+			"TwistedFate",
+			"Twitch",
+			
+			"Udyr",
+			"Urgot",
+			
+			"Varus",
+			"Vayne",
+			"Veigar",
+			//test it
+			"VelKoz",
+			"Vi",
+			"Viktor",
+			"Vladimir",
+			"Volibear",
+			
+			"Warwick",
+			"Wukong",
+			
+			"Xayah",
+			"Xerath",
+			"XinZhao",
+			
+			"Yasuo",
+			"Yorick",
+			
+			"Zac",
+			"Zed",
+			"Ziggs",
+			"Zilean",
+			"Zoe",
+			"Zyra"
+		};
+
+}
