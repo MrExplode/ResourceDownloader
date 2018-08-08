880b6507c960355ae15fe055c1443f346cb9af1f
MrExplode <pjanos@outlook.hu>
2018-08-08 12:41:40 +0200

    Added console interface
    Added some skin group downloads.
    
    Added the console starter.

--- src/me/mrexplode/resdownloader/Main.java
+++ src/me/mrexplode/resdownloader/Main.java
@@ -1,9 +1,11 @@
 package me.mrexplode.resdownloader;
 
 import java.awt.GraphicsEnvironment;
+import java.io.BufferedReader;
 import java.io.Console;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -13,17 +15,56 @@ public class Main {
 	public static String path = "/ChampionData";
 	
 	private static final int maxThread = 5;
-	public static String[] champs = Champions.champList;
 	
-	private static String[] testlist = new String[] {"Darius", "Kindred", "Braum", "Elise", "Viktor", "Evelynn"};
-	
-	public static void main(String[] args) {
-		System.out.print("-----===== MrExplode's Resource Downloader ======-----\nYou must know, due Riot's request limit, the complete download must last around 8 minute.\n Because: The limit says: 20 request per sec, and 100 request per 2 min\nLoL currently have 141 champs, the average skin number is 5,7 so we will download around 800 skin");
+	public static void main(String[] args) throws IOException {
+		if (!startConsole()) return;
+		System.out.print("-----===== MrExplode's Resource Downloader ======-----\nYou must know, due Riot's request limit, the complete download must last around 8 minute.\n Because: The limit says: 20 request per sec, and 100 request per 2 min\nLoL currently have 141 champs, the average skin number is 5,7 so we will download around 800 skin.\n\nPlease down what download type you want. <all | project | blood-moon | star-guardian | arcade>: ");
+		
+		BufferedReader userIn = new BufferedReader(new InputStreamReader(System.in));
+		String response = userIn.readLine();
 		
 		ExecutorService executor = Executors.newFixedThreadPool(maxThread);
-		for (int i = 0; i < testlist.length; i++) {
-			Runnable worker = new Downloader(testlist[i]);
-			executor.execute(worker);
+		
+		switch (response.toLowerCase()) {
+		case "all":
+			System.out.println("Downloading all champion skins, might take long time\n\n");
+			for (Champions champ : Champions.values()) {
+				Runnable worker = new Downloader(champ.name, champ.knownGaps);
+				executor.execute(worker);
+			}
+			break;
+		
+		case "project":
+			System.out.println("Downloading PROJECT skins...\n\n");
+			for (int i = 0; i < Champions.PROJECT.size(); i++) {
+				Runnable worker = new Downloader(Champions.PROJECT.get(i), 0);
+				executor.execute(worker);
+			}
+			break;
+			
+		case "star-guardian":
+			System.out.println("Downloading Star Guardian skins...\n\n");
+			for (int i = 0; i < Champions.STAR_GUARDIAN.size(); i++) {
+				Runnable worker = new Downloader(Champions.STAR_GUARDIAN.get(i), 0);
+				executor.execute(worker);
+			}
+			break;
+			
+		case "blood-moon":
+			System.out.println("Download Blood Moon skins...\n\n");
+			for (int i = 0; i < Champions.BLOOD_MOON.size(); i++) {
+				Runnable worker = new Downloader(Champions.BLOOD_MOON.get(i), 0);
+				executor.execute(worker);
+			}
+			break;
+		
+		case "arcade":
+			System.out.println("Downloading Arcade skins...\n\n");
+			for (int i = 0; i < Champions.ARCADE.size(); i++) {
+				Runnable worker = new Downloader(Champions.ARCADE.get(i), 0);
+				executor.execute(worker);
+			}
+			break;
 		}
 		
 		executor.shutdown();
