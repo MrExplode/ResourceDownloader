e3a4b2c8bd1acd09782beb3889d1abd1b152a5f1
MrExplode <pjanos@outlook.hu>
2018-08-08 12:21:28 +0200

    Completed gap jumping mechanism
    Gap jumping: i noticed that there are some gaps in the riot skin id's.
    for example in the simple way of download, we go until the 403, wich
    means the end of the skins. at MasterYi, we get the error after id 6,
    but there are 2 skins with id 11, and 12.
    
    So, created an enum map at Champions, with gap size list.
    (Also, found 5 champs, where are more than one gap exist)
    
    Now, if we run into a 403 response, we try to jump with the next gap
    number on the list.

--- src/me/mrexplode/resdownloader/Downloader.java
+++ src/me/mrexplode/resdownloader/Downloader.java
@@ -10,9 +10,11 @@ import java.net.URL;
 public class Downloader extends Thread {
 	
 	private String champName;
+	private int[] knownGaps;
 	
-	public Downloader(String champName) {
+	public Downloader(String champName, int... knownGaps) {
 		this.champName = champName;
+		this.knownGaps = knownGaps;
 	}
 
 	@Override
@@ -23,13 +25,55 @@ public class Downloader extends Thread {
 		}
 		
 		System.out.println("Downloading " + champName + "...\nTo: " + cFolder.getAbsolutePath());
+		long start = System.currentTimeMillis();
 		
+		int skins = downloadSequence(champName, cFolder, knownGaps);
+		
+		long end = (System.currentTimeMillis() - start) - (skins * 100 + 100);//+100 for the default skin, the return exclude that
+		System.out.println(champName + " download completed in " + end + "ms with " +skins + " skins (without default)\n");
+	}
+	
+	/**
+	 * Download all skins for the champion.
+	 * 
+	 * @param name The name of the champion.
+	 * @param cFolder The folder to save the images.
+	 * @param gaps The list of the known ID gaps.
+	 * @return The number of the skins for the specified champion, excluding the default.
+	 */
+	public int downloadSequence(String name, File cFolder, int... gaps) {
 		int skinNumber = 0;	
+		int gapNum = 0;
 		File img = new File(cFolder, champName + "_" + skinNumber + ".jpg");
 		
-		while (download("http://ddragon.leagueoflegends.com/cdn/img/champion/splash/" + champName + "_" + skinNumber + ".jpg", img)) {
-			skinNumber++;
-			img  = new File(cFolder, champName + "_" + skinNumber + ".jpg");
+		int var1 = 0;
+		while (true) {
+			if (download("http://ddragon.leagueoflegends.com/cdn/img/champion/splash/" + champName + "_" + skinNumber + ".jpg", img)) {
+				skinNumber++;
+				img  = new File(cFolder, champName + "_" + skinNumber + ".jpg");
+				System.out.println("DEBUG: normal downloaded with number " + (skinNumber - 1));
+			} else {
+				if (knownGaps.length == 1 && knownGaps[0] == 0) {
+					System.out.println("DEBUG: tried to jump the gap, but not specified, thats all");
+					break;
+				} else {
+					skinNumber += knownGaps[gapNum] - 1;
+					System.out.println("DEBUG: successfully jumped gap with amount " + knownGaps[gapNum] + ", now skinNumber = " + skinNumber);
+					if (knownGaps.length-1 > gapNum) {
+						gapNum++;
+						System.out.println("DEBUG: gapNum increased to " + gapNum);
+					} else {
+						if (var1 == 0) {
+							var1++;
+							//if we not increasing, we still need to run after the jump to download
+						} else {
+							System.out.println("DEBUG: end it boi there is no more gap, we can chill");
+							break;
+						}
+					}
+				}
+			}
+			
 			try {
 				Thread.sleep(100);
 			} catch (InterruptedException e) {
@@ -37,9 +81,16 @@ public class Downloader extends Thread {
 			}
 		}
 		
-		System.out.println(champName + " download completed with " + (skinNumber - 1)  + " skins (without default)\n");
+		return skinNumber - 1;
 	}
 	
+	/**
+	 * Downloads a single image from the specified url.
+	 * 
+	 * @param urlName The specified url to the image
+	 * @param path File to save
+	 * @return true if the download succeed, otherwise false. For example if there is no image, the 403 response error will return false
+	 */
 	public boolean download(String urlName, File path) {
 		try {
 			URL url = new URL(urlName);
